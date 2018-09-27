2691e625a1b341c20a89d284846c06289b6e08e5
josh reese <droptablestar@gmail.com>
2018-09-27 16:43:31 -0500

    Changed how key finding loop works.

--- key_finder.py
+++ key_finder.py
@@ -7,11 +7,12 @@ def dump_value(f):
         # find the matching { and that should be the end of this keys value
         brackets = 1
         c = f.read(1)
+
         in_str = False
-        in_arr = False
-        in_obj = False
+        square_count = 0
+        curly_count = 0
+
         while brackets > 0 and c:
-            print('c:', c, in_str)
             if c == '{':
                 brackets += 1
             elif c == '}':
@@ -21,57 +22,45 @@ def dump_value(f):
                 in_str = not in_str
 
             # arrays
-            if not in_str and c == '[':
-                in_arr = True
-            if not in_str and c == ']':
-                in_arr = False
+            if c == '[':
+                square_count += 1
+            if c == ']':
+                square_count -= 1
 
             # objects
-            if not in_str and c == '{':
-                in_obj = True
-            if not in_str and c == '}':
-                in_obj = False
+            if c == '{':
+                curly_count += 1
+            if c == '}':
+                curly_count -= 1
 
-            if not in_str and not in_arr and not in_obj and c == ',':
+            if square_count == 0 and curly_count == 0 and not in_str and c == ',':
                 brackets = 0
             if brackets > 0:
                 fd.write(c)
             c = f.read(1)
 
 # key to find
-key = 'GlossList'
+KEY = 'GlossSeeAlso'
 data = 'test1.json'
 
 tokens = ''
 with open(data) as f:
     c = f.read(1)
+
     in_str = False
+    tokens = ''
     while(c):
-        # skip escaped characters and spaces
-        if c == '\\' or c == '{' or c == '}':
-            c = f.read(1)
-            continue
-        print('c:', c, in_str)
-        tokens += c
-        print ('token:', tokens)
         if c == '"':
-            in_str = True
-            c = f.read(1)
-            if c == ':':
-                in_str = False
-                tokens = tokens.strip().strip('"')
-                print('KEY: ', tokens)
-                if tokens == key:
-                    # tokens should contain the whole key now, as well as leading and
-                    # trailing "
-                    dump_value(f)
-                    break
-                tokens = ''
-            elif c == ',' or c == '}' or c == ']':
-                in_str = False
-                tokens = ''
-            else:
-                tokens += c
-        if not in_str and c == ',':
+            in_str = not in_str
+        if in_str:
+            tokens += c
+        if c == ',' and not in_str:
             tokens = ''
+        if c == ':' and not in_str:
+            key = tokens.strip().strip('"')
+            if KEY == key:
+                dump_value(f)
+                break
+            else:
+                tokens = ''
         c = f.read(1)
